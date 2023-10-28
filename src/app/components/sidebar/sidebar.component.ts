015d955d8737a1da345f0dc5eb70027f4bc9b654
arisa_UTCC <arisa_koo@utcc.ac.th>
2023-10-28 23:22:40 +0700

    edit layout

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.css']
+  styleUrls: ['./sidebar.component.css'],
 })
 export class SidebarComponent {
+  @Input() menu: any;
+  constructor(private router: Router) {}
 
+  value = [
+    {
+      name: 'List',
+      path: '/work-list',
+      count: 1,
+    },
+    {
+      name: 'Approve',
+      path: '/approve',
+      count: 13,
+    },
+    {
+      name: 'Reject',
+      path: '/reject',
+      count: 94,
+    },
+  ];
+  ngOnInit(): void {}
+
+  goTo(path: string): void {
+    this.router.navigate([path]);
+  }
 }
